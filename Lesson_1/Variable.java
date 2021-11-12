e2955091657cee9cf5d2cfca335e5b28098ff5ef
GlebCoder <gberliand@gmail.com>
2021-11-12 15:59:21 +0300

    exercises 1 and 2

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte cpuNumbers = 8;
+        System.out.println(cpuNumbers);
+        short modelNumber = 140;
+        System.out.println(modelNumber);
+        int memory = 8;
+        System.out.println(memory);
+        long disk = 512L;
+        System.out.println(disk);
+        float processor = 2.4f;
+        System.out.println(processor);
+        double maxFrequency = 4200.00;
+        System.out.println(maxFrequency);
+        char justLetter = 'G';
+        System.out.println(justLetter);
+        boolean hasComputer = true;
+        System.out.println(hasComputer);
+
+    }
+}
