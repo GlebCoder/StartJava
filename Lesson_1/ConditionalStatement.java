c59cb9f4fe323a98be515f9c70c5cd1105626b5a
GlebCoder <gberliand@gmail.com>
2021-11-13 14:35:18 +0300

    ConditionalStatement correction

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,30 +1,32 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 5;
-        boolean isMale = false;
-        double height = 1.75;
-        char nameFirstLetter = 'G';
-
         if (age > 20) {
             System.out.println("You are older than 20");
         }
+
+        boolean isMale = false;
         if (isMale) {
             System.out.println("You are not a woman");
         }
         if (!isMale) {
             System.out.println("You are not a man");
         }
+
+        double height = 1.75;
         if (height < 1.80) {
             System.out.println("You are not so tall");
         } else {
             System.out.println("You are so tall");
         }
-        if (nameFirstLetter == 'M') {
+
+        char firstLetterName = 'G';
+        if (firstLetterName == 'M') {
             System.out.println("Your name starts with 'M'");
-        } else if (nameFirstLetter == 'I') {
+        } else if (firstLetterName == 'I') {
             System.out.println("Your name starts with 'I'");
         } else {
-            System.out.println("Your hame does not start with 'M' or 'I'");
+            System.out.println("Your name does not start with 'M' or 'I'");
         }
     }
 } 
