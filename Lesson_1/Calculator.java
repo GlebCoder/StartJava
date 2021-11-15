ecbc89553d949f29a67050ef773a5f7b179773d4
GlebCoder <gberliand@gmail.com>
2021-11-15 16:57:08 +0300

    Calculator corrections and solution for a guess number game

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int result = 1;
 
         System.out.println("Please, enter the first positive integer number:");
         int firstNumber = scanner.nextInt();
 
-        System.out.println("Please, enter an operation sign. It can be: '+', '-', '/', '%', '^'");
+        System.out.println("Please, enter an operation sign. It can be: '+', '-', '/', '%', '^', '*'");
         String operation = scanner.next();
 
         System.out.println("Please, enter the second positive integer number:");
         int secondNumber = scanner.nextInt();
 
+        int result = 0;
         if (operation.equals("+")) {
             result = firstNumber + secondNumber;
         } else if (operation.equals("-")) {
@@ -23,9 +23,12 @@ public class Calculator {
         } else if (operation.equals("%")) {
             result = firstNumber % secondNumber;
         } else if (operation.equals("^")) {
+            result = 1;
             for (int i = 1; i <= secondNumber; i++) {
                 result *= firstNumber;
             }
+        } else if (operation.equals("*")) {
+            result = firstNumber * secondNumber;
         }
         System.out.println("The operation result: " + result);
     }
---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,22 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int min = 0;
+        int max = 100;
+        int randomNumber = 23;
+        int guessNumber = 0;
+
+        while (guessNumber != randomNumber) {
+            guessNumber = (min + max) / 2;
+            System.out.println("My guess is " + guessNumber);
+            if (guessNumber < randomNumber) {
+                System.out.println("Your number is too small");
+                min = guessNumber;
+            }
+            if (guessNumber > randomNumber) {
+                System.out.println("Your number is too big");
+                max = guessNumber;
+            }
+        }
+        System.out.println("Congratulations!!! You have found the right answer!");
+    }
+}
