cd8b41fb8be9b794e50daf0eaf82106a28c7d7de
GlebCoder <gberliand@gmail.com>
2021-11-17 10:11:39 +0300

    solution for modified wolf and wolfTest exercise

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,9 +1,53 @@
 public class Wolf {
-    public char sex;
-    public String name;
-    public double weight;
-    public int age;
-    public String color;
+    private char sex;
+    private String name;
+    private double weight;
+    private int age;
+    private String color;
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Incorrect age");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     public void walk() {
         System.out.println("Walking...");
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,17 +1,17 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.name = "Alone";
-        wolf.color = "grey";
-        wolf.age = 7;
-        wolf.weight = 19.2;
-        wolf.sex = 'M';
+        wolf.setName("Alone");
+        wolf.setColor("grey");
+        wolf.setAge(7);
+        wolf.setWeight(19.2);
+        wolf.setSex('M');
         
-        System.out.println("Wolf's name: " + wolf.name 
-                + "\nColor: " + wolf.color
-                + "\nAge: " + wolf.age
-                + "\nWeight: " + wolf.weight
-                + "\nSex: " + wolf.sex);
+        System.out.println("Wolf's name: " + wolf.getName() 
+                + "\nColor: " + wolf.getColor()
+                + "\nAge: " + wolf.getAge()
+                + "\nWeight: " + wolf.getWeight()
+                + "\nSex: " + wolf.getSex());
         
         wolf.run();
         wolf.howl();
