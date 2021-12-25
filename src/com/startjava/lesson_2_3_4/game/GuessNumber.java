e6da0b792583a60ddf58b8c127a75400d9fd9d1d
GlebCoder <gberliand@gmail.com>
2021-12-25 10:44:30 +0300

    GuessNumber has been changed according to the 4th lesson

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -18,20 +18,46 @@ public class GuessNumber {
         String player2Name = player2.getName();
         boolean isFound = false;
         int hiddenNumber = getHiddenNumber();
+        int count = 0;
+
+        // The "flag" variable is used to show different numbers of attempts if the first player
+        // has found the hidden number.
+        int flag = 0;
+        System.out.println("Dear players you have 10 attempts only to find the number!");
         do {
             System.out.println(player1Name + ", enter a number");
             int playerGuess = scanner.nextInt();
+            player1.setAttempt(count, playerGuess);
             isFound = compare(playerGuess, hiddenNumber);
+            if (!isFound && count == 9) {
+                System.out.println(player1Name + ", you have used all your attempts");
+            }
             if (!isFound) {
                 System.out.println(player2Name + ", enter a number");
                 playerGuess = scanner.nextInt();
+                player2.setAttempt(count, playerGuess);
                 isFound = compare(playerGuess, hiddenNumber);
+                if(!isFound && count == 9) {
+                    System.out.println(player2Name + ", you have used all your attempts");
+                } else if (isFound) {
+                    System.out.println(player2Name + ", you have found the number with " + (count + 1) + " attempts!!!");
+                    break;
+                }
             } else {
-                System.out.println(player1Name + ", you have found the number!!!");
-                return;
+                flag = 1;
+                System.out.println(player1Name + ", you have found the number with " + (count + 1) + " attempts!!!");
+                break;
             }
-        } while (!isFound);
-        System.out.println(player2Name + ", you have found the number!!!");
+            count++;
+        } while (!isFound && count < 10);
+
+        int[] player1Attempts = player1.getAttempts(count);
+        player1.printAttempts(player1Attempts);
+        int[] player2Attempts = player2.getAttempts(count - flag);
+        player2.printAttempts(player2Attempts);
+
+        player1.clearAttempts();
+        player2.clearAttempts();
     }
 
     private boolean compare(int playerGuess, int hiddenNumber) {
--- src/com/startjava/lesson_2_3_4/game/Player.java
+++ src/com/startjava/lesson_2_3_4/game/Player.java
@@ -1,8 +1,10 @@
 package com.startjava.lesson_2_3_4.game;
 
+import java.util.Arrays;
+
 public class Player {
     private String name;
-    private int count;
+    private int[] attempts = new int[10];
 
     public Player(String name) {
         this.name = name;
@@ -12,11 +14,20 @@ public class Player {
         return name;
     }
 
-    public int getCount() {
-        return count;
+    public int[] getAttempts(int count) {
+        return Arrays.copyOf(attempts, count+1);
     }
 
-    public void setCount(int count) {
-        this.count = count;
+    public void setAttempt(int index, int playerGuess) {
+        attempts[index] = playerGuess;
+    }
+
+    public void printAttempts(int[] attempts) {
+        System.out.print(name + ", your attempts were: ");
+        System.out.println(Arrays.toString(attempts));
+    }
+
+    public void clearAttempts() {
+        Arrays.fill(attempts, 0);
     }
 }
